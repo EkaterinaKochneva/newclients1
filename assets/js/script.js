1ba6f5aa734570d188d63c0aaaabebfa645bf2b8
EkaterinaKochneva <kochnevaes@gmail.com>
2021-10-06 18:03:03 +0300

    06.10

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+$(function () {
+    $(".before-after").twentytwenty({
+        no_overlay: true
+    });
+  });
+
+$(document).ready(function(){
+    $('.cases-slider').bxSlider({
+        touchEnabled: false,
+        infiniteLoop: true,
+        pager: false
+    });
+ });
